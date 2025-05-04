1b71527145a564f4bad5314784c21151ae6a7a99
Bob Kozdemba <bkozdemba@gmail.com>
2025-05-04 17:02:32 +0000

    changed default model

--- config.py
+++ config.py
@@ -6,8 +6,7 @@ https://www.youtube.com/@DevTechBytes
 class Config:
     PAGE_TITLE = "Streamlit Ollama Chatbot"
 
-    OLLAMA_MODELS = ('codellama:7b', 'codellama:13b', 'llama2-uncensored', 
-                    'llama2:7b', 'llama2:13b', 'mistral', 'mixtral')
+    OLLAMA_MODELS = ('qwen3:4b', 'qwen3:0.6b', 'qwen3:1.7b', 'qwen3:8b')
 
     SYSTEM_PROMPT = f"""You are a helpful chatbot that has access to the following 
                     open-source models {OLLAMA_MODELS}.
